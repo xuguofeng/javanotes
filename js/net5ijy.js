2def27ca0349377a83e54d2ed45ebfe051e54db3
xuguofeng <xuguofeng1990@126.com>
2018-04-14 09:13:24 +0800

    提交JDBC第2节PreparedStatement

--- js/net5ijy.js
+++ js/net5ijy.js
@@ -30,19 +30,9 @@ $(function(){
 
 	// 添加"JAVA Web"菜单
 	$("<li></li>")
-		.append($("<a></a>").attr("href", "http://5ijy01.duapp.com/it/javaweb/index.html").html("JAVA WEB"))
+		.append($("<a></a>").attr("href", "/it/javaweb/index.html").html("JAVA WEB"))
 		.insertAfter($("#navbar-nav .navbar-nav .dropdown").eq(0).find("ul.dropdown-menu").find("li").eq(1));
 
+	// 添加分享
 	$("body").append($("<script>window._bd_share_config={\"common\":{\"bdSnsKey\":{},\"bdText\":\"\",\"bdMini\":\"2\",\"bdMiniList\":false,\"bdPic\":\"\",\"bdStyle\":\"0\",\"bdSize\":\"16\"},\"slide\":{\"type\":\"slide\",\"bdImg\":\"5\",\"bdPos\":\"right\",\"bdTop\":\"100\"},\"image\":{\"viewList\":[\"qzone\",\"tsina\",\"tqq\",\"renren\",\"weixin\"],\"viewText\":\"分享到：\",\"viewSize\":\"16\"},\"selectShare\":{\"bdContainerClass\":null,\"bdSelectMiniList\":[\"qzone\",\"tsina\",\"tqq\",\"renren\",\"weixin\"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>"));
-
-/*
-	window.onscroll = function() { 
-		var t = document.documentElement.scrollTop || document.body.scrollTop;
-		var top_div = document.getElementById("CfloatBtn");
-		if(t >= 50)
-			top_div.style.display = "block";
-		else
-			top_div.style.display = "none";
-	};
-*/
 });
